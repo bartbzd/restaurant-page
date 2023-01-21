b5073458093858c45ee59faf674112f33db7a271
Bart Bieszczad <bbart318@gmail.com>
2023-01-21 02:30:35 -0600

    Test page link

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("I'm connected, everything works!")
+import './styles.css';
+// import printMe from './print.js';
+
+const header = () => {
+  const nav = document.createElement('div');
+  const p = document.createElement('p');
+  document.body.appendChild(nav);
+  nav.appendChild(p);
+  nav.classList.add('nav-bar');
+  p.textContent = 'Tester';
+};
+header();
+// eslint-disable-next-line no-console
+console.log('Test log');
