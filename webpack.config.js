cf9651a5d7c5fec792b11c9f39300b2dad349363
Bart Bieszczad <bbart318@gmail.com>
2023-01-21 02:29:29 -0600

    Configure plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const path = require('path')
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,6 +8,38 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'coco.',
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
   mode: 'development',
   devtool: 'inline-source-map',
-}
+  devServer: {
+    static: './dist',
+  },
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
