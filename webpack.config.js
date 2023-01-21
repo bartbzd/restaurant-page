47ce9db38c604cacefe5be9fca1892be95f684b9
Bart Bieszczad <bbart318@gmail.com>
2023-01-21 16:32:50 -0600

    Add image support

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,10 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
