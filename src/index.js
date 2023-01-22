0698f3e13ecc892b7c16d24340311221a673539d
Bart Bieszczad <bbart318@gmail.com>
2023-01-21 23:23:13 -0600

    Import header and append to document

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import './styles.css';
 // import printMe from './print.js';
+import header from './header.js';
 
-const header = () => {
-  const nav = document.createElement('div');
-  const navItems = document.createElement('div');
-  const text = () => document.createElement('p');
-
-  document.body.appendChild(nav);
-  nav.appendChild(text());
-  nav.appendChild(navItems);
-  nav.classList.add('nav-bar');
-  navItems.classList.add('nav-items');
-
-  navItems.appendChild(text()).classList.add('home');
-  navItems.appendChild(text()).classList.add('menu');
-  navItems.appendChild(text()).classList.add('contact');
-
-  document.querySelector('.home').textContent = 'HOME';
-  document.querySelector('.menu').textContent = 'MENU';
-  document.querySelector('.contact').textContent = 'CONTACT';
-};
-header();
-// eslint-disable-next-line no-console
-console.log('Test log');
+const content = document.querySelector('#content');
+content.appendChild(header());
