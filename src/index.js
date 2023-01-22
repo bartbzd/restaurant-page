241c27d72b4ae3edeb974203e0f7a4fb43fa7308
Bart Bieszczad <bbart318@gmail.com>
2023-01-22 14:27:33 -0600

    Import leaf img and attach to content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import './styles.css';
+import Leaf from './img/only-plant.png';
 // import printMe from './print.js';
-import header from './header.js';
+import header from './header';
+import home from './home';
 
 const content = document.querySelector('#content');
 content.appendChild(header());
+content.appendChild(home());
+
+const leaf = new Image();
+leaf.classList.add('leaf');
+leaf.src = Leaf;
+
+content.appendChild(leaf);
