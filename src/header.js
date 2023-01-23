dfb12cafa667286ccd874d33eff9761a73b18442
Bart Bieszczad <bbart318@gmail.com>
2023-01-22 19:24:49 -0600

    Add github icon and top leaf

--- src/header.js
+++ src/header.js
@@ -1,4 +1,5 @@
 import Logo from './img/logo.png';
+import Gitleaf from './img/git-plant.png';
 
 export default function header() {
   const nav = document.createElement('div');
@@ -7,6 +8,10 @@ export default function header() {
   const navItems = document.createElement('div');
   const text = () => document.createElement('p');
 
+  const ghLink = document.createElement('a');
+  ghLink.classList.add('github-link');
+  const ghIcon = document.createElement('i');
+  ghIcon.classList.add('fab', 'fa-github', 'fa-2x');
   nav.appendChild(logo);
   nav.appendChild(navItems);
   nav.classList.add('nav-bar');
@@ -20,6 +25,16 @@ export default function header() {
   nav.querySelector('.home').textContent = 'HOME';
   nav.querySelector('.menu').textContent = 'MENU';
   nav.querySelector('.contact').textContent = 'CONTACT';
+
+  const gitLeaf = new Image();
+  gitLeaf.classList.add('git-leaf');
+  gitLeaf.src = Gitleaf;
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+  nav.appendChild(wrapper);
+  wrapper.appendChild(gitLeaf);
+  wrapper.appendChild(ghLink);
+  ghLink.appendChild(ghIcon);
 
   return nav;
 }
