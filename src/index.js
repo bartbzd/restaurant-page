aad9ba2c8c113ef60b4b31f72a96ea8ff458b9e4
Bart Bieszczad <bbart318@gmail.com>
2023-01-22 22:56:53 -0600

    Reorder js file load

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import './styles.css';
 import Leaf from './img/only-plant.png';
+import Cup from './img/only-cup.png';
 
 import header from './header';
 import home from './home';
 
 const content = document.querySelector('#content');
-content.appendChild(header());
-content.appendChild(home());
-
 const leaf = new Image();
+const cup = new Image();
 leaf.classList.add('leaf');
 leaf.src = Leaf;
+cup.classList.add('cup');
+cup.src = Cup;
+
+content.appendChild(cup);
+content.appendChild(header());
+content.appendChild(home());
 content.appendChild(leaf);
