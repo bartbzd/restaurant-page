96959f5357db91296731bde48f3114864c67fd47
Bart Bieszczad <bbart318@gmail.com>
2023-01-22 19:25:43 -0600

    Remove reference comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.css';
 import Leaf from './img/only-plant.png';
-// import printMe from './print.js';
+
 import header from './header';
 import home from './home';
 
@@ -11,5 +11,4 @@ content.appendChild(home());
 const leaf = new Image();
 leaf.classList.add('leaf');
 leaf.src = Leaf;
-
 content.appendChild(leaf);
