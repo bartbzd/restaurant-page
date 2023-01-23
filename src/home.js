5c6f3ffac252bef3c080076596f34c4b59d62780
Bart Bieszczad <bbart318@gmail.com>
2023-01-23 03:18:03 -0600

    Add store finder section

--- src/home.js
+++ src/home.js
@@ -13,5 +13,18 @@ export default function home() {
   container.appendChild(hero);
   container.appendChild(btn);
 
+  const storeWrapper = document.createElement('div');
+  const hours = document.createElement('p');
+  const map = document.createElement('a');
+  const mapIcon = document.createElement('i');
+  storeWrapper.classList.add('store-wrapper');
+  hours.classList.add('hours');
+  hours.textContent = 'Find a store';
+  mapIcon.classList.add('fa-solid', 'fa-location-dot', 'fa-1x');
+  map.appendChild(mapIcon);
+  storeWrapper.appendChild(map);
+  storeWrapper.appendChild(hours);
+  container.appendChild(storeWrapper);
+
   return container;
 }
