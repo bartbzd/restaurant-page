6f96383358f47e5dff4ba2c6d85ea588abdeb92b
Bart Bieszczad <bbart318@gmail.com>
2023-01-24 01:58:14 -0600

    Add container and nav menu listener

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import './styles.css';
 import Leaf from './img/only-plant.png';
 import Cup from './img/only-cup.png';
 
-import header from './header';
+import header, { gitLeaf } from './header';
 import home from './home';
+import menu from './menu';
 
-const content = document.querySelector('#content');
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+content.classList.add('main-container');
+
 const leaf = new Image();
 const cup = new Image();
 leaf.classList.add('leaf');
@@ -13,7 +17,29 @@ leaf.src = Leaf;
 cup.classList.add('cup');
 cup.src = Cup;
 
-content.appendChild(cup);
-content.appendChild(header());
+container.appendChild(header());
+container.appendChild(content);
+container.appendChild(gitLeaf());
 content.appendChild(home());
+content.appendChild(cup);
 content.appendChild(leaf);
+
+const homePage = document.querySelector('.logo');
+const menuItem = document.querySelector('.menu');
+
+homePage.addEventListener('click', () => {
+  content.innerHTML = '';
+  content.appendChild(home());
+  content.appendChild(cup);
+  content.appendChild(leaf);
+  container.querySelector('.menu', '.about', '.contact').classList.toggle('active');
+});
+menuItem.addEventListener('click', () => {
+  content.innerHTML = '';
+  content.appendChild(menu());
+  container.querySelector('.menu').classList.toggle('active');
+});
+
+//Add menu content, img and text cards
+//Animate bottom leaf position on menu page load
+//Add nav item selected styling
