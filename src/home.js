b736fc7f5a0a72cce5c0b011c5698ed877a946e5
Bart Bieszczad <bbart318@gmail.com>
2023-01-27 01:16:54 -0600

    Move Find Store wrapper

--- src/home.js
+++ src/home.js
@@ -11,19 +11,5 @@ export default function home() {
   btn.append('Order Online');
   container.append(hero, btn);
 
-  const storeWrapper = document.createElement('div');
-  storeWrapper.classList.add('store-wrapper');
-  const text = document.createElement('p');
-  text.append('Find a store');
-  text.className = 'store';
-  const a = document.createElement('a');
-  const i = document.createElement('i');
-  i.className = 'fa-solid fa-location-dot fa-1x';
-
-  container.appendChild(storeWrapper);
-  storeWrapper.appendChild(a);
-  storeWrapper.appendChild(text);
-  a.appendChild(i);
-
   return container;
 }
